8fe97da9b76f9b078eae70528506d603d0154517
zjxjohnxtlxb <zjxjohnxtlxb@outlook.com>
2021-08-19 11:00:07 +0200

    fix bugs

--- Frontend/app/javascript/packs/resultat.js
+++ Frontend/app/javascript/packs/resultat.js
@@ -1,20 +1,28 @@
 /*
  * @Date: 2021-07-31 20:38:07
  * @LastEditors: Junxi ZHANG
- * @LastEditTime: 2021-07-31 21:36:25
+ * @LastEditTime: 2021-08-19 08:19:34
  * @FilePath: /Frontend/app/javascript/packs/resultat.js
  */
 document.addEventListener('turbolinks:load', () => {
     const click1 = document.getElementById("a_per");
     click1.addEventListener('click', (event) => {
-        window.frames['option'].src='per'
+        remove_default();
+        window.frames['option'].src='per';
     });
     const click2 = document.getElementById("a_cog");
     click2.addEventListener('click', (event) => {
-        window.frames['option'].src='cog'
+        remove_default();
+        window.frames['option'].src='cog';
     });
     const click3 = document.getElementById("a_soc");
     click3.addEventListener('click', (event) => {
-        window.frames['option'].src='soc'
+        remove_default();
+        window.frames['option'].src='soc';
     });
 });
+
+function remove_default(){
+    const label = document.getElementById("a_cog").nextElementSibling;
+    label.classList.remove("default-label");
+}
